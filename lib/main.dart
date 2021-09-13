fac0c2f6741fb873051650e4051d7c1e4c4ad4c9
unknown <marius.hagelin@gmail.com>
2021-09-13 19:11:48 +0200

    Split up the widgets, added scores and end screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/result.dart';
 
-import './question.dart';
-import './answer.dart';
+import './result.dart';
+import './quiz.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,48 +16,66 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'Who\'s your favorite Daedra?',
+      'answerText': [
+        {'text': 'Clavicus Vile', 'score': 3},
+        {'text': 'Hermaeus Mora', 'score': 1},
+        {'text': 'Mephala', 'score': 6},
+        {'text': 'Nocturnal', 'score': 10}
+      ]
+    },
+    {
+      'questionText': 'Who\'s your favorite Aedra?',
+      'answerText': [
+        {'text': 'Arkay', 'score': 1},
+        {'text': 'Julianos', 'score': 4},
+        {'text': 'Stendarr', 'score': 7},
+        {'text': 'Zenithar', 'score': 10}
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite City?',
+      'answerText': [
+        {'text': 'Chorrol', 'score': 8},
+        {'text': 'Cheydinhal', 'score': 10},
+        {'text': 'Leyawiin', 'score': 4},
+        {'text': 'Bravil', 'score': 1}
+      ]
+    }
+  ];
   var _questionIndex = 0;
 
-  void _answerQuestion() {
+  var _totalScore = 0;
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
-
     print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print("We have more questions!");
+    } else {
+      print("No more questions!");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'Who\'s your favorite Daedra?',
-        'answerText': ['Clavicus Vile', 'Hermaeus Mora', 'Mephala', 'Nocturnal']
-      },
-      {
-        'questionText': 'Who\'s your favorite Aedra?',
-        'answerText': ['Arkay', 'Julianos', 'Stendarr', 'Zenithar']
-      },
-      {
-        'questionText': 'What\'s your favorite City?',
-        'answerText': ['Chorrol', 'Cheydinhal', 'Leyawiin', 'Bravil']
-      }
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My quiz app'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex]['questionText'],
-            ),
-            ...(questions[_questionIndex]['answerText'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore),
       ),
     );
   }
---
+++ lib/result.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'Good job, you know a little!';
+    } else if (resultScore <= 12) {
+      resultText = 'Nice, you payed attention!';
+    } else if (resultScore <= 16) {
+      resultText = 'Almost perfect!';
+    } else {
+      resultText = 'Oh damn, are you me?';
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text(
+      resultPhrase,
+      style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      textAlign: TextAlign.center,
+    ));
+  }
+}
